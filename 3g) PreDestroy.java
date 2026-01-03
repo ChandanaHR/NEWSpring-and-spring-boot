0dc584d412fc16cb7527aa8018e1e1d4566ae57e
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-01-03 11:51:39 +0530

    Create 3g) PreDestroy.java

---
+++ 3g) PreDestroy.java
@@ -0,0 +1,64 @@
+//Student.java
+package com.example1.demo1;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+public class Student {
+	public Student() {
+		System.out.println("1. Student object created");
+	}
+	@PostConstruct
+	public void init() {
+		System.out.println("3. Student initialized");
+	}
+	@PreDestroy
+	public void destroy() {
+		System.out.println("4. Student destroyed");
+	}
+}
+
+//Main.java
+package com.example1.demo1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		Student t = context.getBean(Student.class);
+//		t.notifyuser("sms");
+		((AbstractApplicationContext) context).close();
+    }
+}
+
+//Beanpostprocess
+package com.example1.demo1;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Beanpostprocess implements BeanPostProcessor {
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) {
+		if(beanName.equals("student")) {
+			System.out.println("2. Student check before initialization");
+		}
+		return bean;
+	}
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) {
+		if(beanName.equals("student")) {
+			System.out.println("4. Student check after initialization");
+		}
+		return bean;
+	}
+}
