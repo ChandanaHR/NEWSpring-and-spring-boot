0fba4459d45aedae591167f970c78f97ad7bed41
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-02-19 07:44:44 +0530

    Update m5) HTTP Methods of Request mapping.java

--- m5) HTTP Methods of Request mapping.java
+++ m5) HTTP Methods of Request mapping.java
@@ -9,3 +9,33 @@ public class UserController {
     }
 }
 http://localhost:8080/api/users
+
+POST: REQUEST
+    Create User.java in eclipse
+    package com.example.mvc1;
+
+public class User {
+	private String name;
+	private String email;
+	
+	public User() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
+
