3bdb8d440511383320dd538023113a0f5b9d5320
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-02-20 17:49:57 +0530

    Update m5) HTTP Methods of Request mapping.java

--- m5) HTTP Methods of Request mapping.java
+++ m5) HTTP Methods of Request mapping.java
@@ -38,4 +38,105 @@ public class User {
 	
 	
 }
+Create controller
+	package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")   // React Vite port
+public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user) {
+        users.add(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return users;
+    }
+}
+React code
+	import { useState, useEffect } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+
+  // Load users
+  const loadUsers = () => {
+    fetch("http://localhost:8080/api/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  };
+
+  // Add user (POST)
+  const addUser = () => {
+    fetch("http://localhost:8080/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      setUsers([...users, data]);
+      setName("");
+      setEmail("");
+    });
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Add User</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={addUser}>Submit</button>
+
+      <hr />
+
+      <h2>User List</h2>
+      <button onClick={loadUsers}>Load Users</button>
+
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
 
