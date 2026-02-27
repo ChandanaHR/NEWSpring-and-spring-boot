25d4cf113aa51bb3b8e74d69d7f014fc20987e9b
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-02-27 07:10:46 +0530

    Update m7 Request data handling.java

--- m7 Request data handling.java
+++ m7 Request data handling.java
@@ -11,3 +11,15 @@ public class StudentController {
         return "Name: " + name + ", Age: " + age;
     }
 }
+
+// @PathVariable
+    @Controller
+public class StudentController {
+
+    @GetMapping("/student/{id}")
+    @ResponseBody
+    public String getStudentById(@PathVariable int id) {
+
+        return "Student ID: " + id;
+    }
+}
