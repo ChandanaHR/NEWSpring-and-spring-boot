145db4dbb2ebad0e83f5c5ce953dc67ee29152c9
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-06-15 18:59:21 +0530

    Create RestHateoasasLinks.java

---
+++ RestHateoasasLinks.java
@@ -0,0 +1,53 @@
+//Hateoas
+package mvc.demo1;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+@RequestMapping("/studentshateoas")
+public class Hateoaslinks {
+	@GetMapping("/{id}")
+	public EntityModel<Student1> getStudent(@PathVariable Integer id) {
+		Student1 student = new Student1(id, "Chandana");
+		EntityModel<Student1> model = EntityModel.of(student);
+		
+		model.add(linkTo(methodOn(Hateoaslinks.class).getStudent(id)).withSelfRel());
+		return model;
+	}
+}
+//Student1
+package mvc.demo1;
+
+public class Student1 {
+	private Integer id;
+	private String name;
+	
+	public Student1() {
+		
+	}
+	public Student1(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
