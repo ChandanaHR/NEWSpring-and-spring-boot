0cccef9bf94ded65b6087435ad80e5c6949460ec
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-09-01 23:10:51 +0530

    Update paginationandsortingspringdatajpa.java

--- paginationandsortingspringdatajpa.java
+++ paginationandsortingspringdatajpa.java
@@ -59,3 +59,113 @@ public class EmployeeController {
 }
 GET /employees?page=0&size=5
   GET /employees?department=IT&page=0&size=2
+
+  p2) Useful page methods
+  package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String department;
+
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+Repository
+  package com.example.demo.repository;
+
+import com.example.demo.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository
+        extends JpaRepository<Employee, Long> {
+
+    Page<Employee> findByDepartment(
+            String department,
+            Pageable pageable
+    );
+}
+Service
+  @Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public Page<Employee> getEmployees(
+            int page,
+            int size) {
+
+        Pageable pageable =
+                PageRequest.of(page, size);
+
+        return repository.findAll(pageable);
+    }
+}
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping
+    public Page<Employee> getEmployees(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+
+        return service.getEmployees(page, size);
+    }
+}
+
